9ef7508d31c7feacba945f617d8b06b8e177e449
praveenarupi <praveenarupi@gmail.com>
2022-10-27 17:38:11 +0530

    updating ./terraform-modules/tf-module-alb

--- main.tf
+++ main.tf
@@ -65,6 +65,13 @@ resource "aws_security_group" "public" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress {
+    description = ""
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
   tags = {
     Name = "${var.env}-public-alb"
   }
